b477f3994aa5a6b301b2347a2e6fce42d05a9ddd
tolotrasam <tolotrasam@gmail.com>
2016-06-01 18:41:09 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo '<li><a href = "about.php">about</a></li>' ;
 echo '<li><a href = "contact.html">contact</a></li>' ;
 echo '<li><a href = "demo-form.php">demoform</a></li>' ;
+echo 'copyright tolotra';
 ?>
