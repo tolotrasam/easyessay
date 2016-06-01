5a584f8753e897c4ce250b0e8ae50e542a6dc77a
tolotrasam <tolotrasam@gmail.com>
2016-06-01 18:29:48 +0400

    Add files via upload

---
+++ demo-form.php
@@ -0,0 +1,5 @@
+<form action="demo.php" method="post" />
+<p>Input 1: <input type="text" name="input1" /> </P>
+<p>Input 2: <input type="text" name="input2" /> </P>
+<input type="submit" value="Submit" />
+</form>
