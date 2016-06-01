d537f8a5e7391a8bffbe1d1fdf1fa71daf9fa938
tolotrasam <tolotrasam@gmail.com>
2016-06-01 18:37:56 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php echo 'it works!' ;
-echo '<a href = "about.php">about</a>' ;
-echo '<a href = "contact.html">contact</a>' ;
-echo '<a href = "demo-form.php">demoform</a>' ;
+echo '<li><a href = "about.php">about</a></li>' ;
+echo '<li><a href = "contact.html">contact</a></li>' ;
+echo '<li><a href = "demo-form.php">demoform</a></li>' ;
 ?>
