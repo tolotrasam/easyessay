fcb126cd3e817146c339a0f0272a0e483d9adef2
tolotrasam <tolotrasam@gmail.com>
2016-06-02 11:44:10 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php echo 'it works!' ;
+<<<<<<< HEAD
+echo '<a href = "about.php">about</a>' ;
+echo '<a href = "contact.html">contact</a>' ;
+echo '<a href = "mainvisual.html">visual board</a>' ;
+
+
 echo '<li><a href = "about.php">about</a></li>' ;
 echo '<li><a href = "contact.html">contact</a></li>' ;
 echo '<li><a href = "demo-form.php">demoform</a></li>' ;
 echo 'copyright tolotra';
 require 'contact.html';
 ?>
+
