62355deeb29957a6ac132b4ba2abfd406c18c9ac
tolotrasam <tolotrasam@gmail.com>
2017-04-29 16:31:46 +0400

    First push

--- boardpicture/demo-form.php
+++
@@ -1,5 +0,0 @@
-<form action="demo.php" method="post" />
-<p>Input 1: <input type="text" name="input1" /> </P>
-<p>Input 2: <input type="text" name="input2" /> </P>
-<input type="submit" value="Submit" />
-</form>
